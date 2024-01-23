c9e0a22615aa2ddf79cca88f9385395edc228a54
AnilAntari <retro201272@gmail.com>
2024-01-23 20:28:59 +0300

    update attention.pl

--- attention.pl
+++ attention.pl
@@ -1,45 +1,52 @@
-#!/usr/bin/perl
-
-use 5.010;
 use strict;
 use warnings;
+use 5.010;
 
 my $directory = '/var/uwuscan_log';
 my $warning_sent = 0;
 
-my @files = glob("$directory/*.log");
+opendir(my $dh, $directory);
+my @files = readdir($dh);
 
 foreach my $filename (@files) {
-  if (open(my $fh, '<:encoding(UTF-8)', $filename)) {
-    while (my $row = <$fh>) {
-      chomp $row;
-      my @fields = split(' ', $row);
-      my $cartridge = $fields[6];
-      my $drum = $fields[7];
+    next unless ($filename =~ /\.log$/);
+    $filename = "$directory/$filename";
 
-      if ($cartridge < 20 || $drum < 20) {
-        if(!$warning_sent){
-          & telegram_attention;
-          $warning_sent =1;  
+    if (open(my $fh, '<:encoding(UTF-8)', $filename)) {
+        my @lines = reverse <$fh>;
+        foreach my $row (@lines) {
+            chomp $row;
+            my @fields = split(' ', $row);
+            my $cartridge = $fields[6];
+            my $drum = $fields[7];
+
+            if ($cartridge < 20) {
+                if (!$warning_sent) {
+                    telegram_attention();
+                    $warning_sent = 1;
+                }
+                
+            } elsif ($drum < 20) {
+                    if (!$warning_sent) {
+                        telegram_attention();
+                        $warning_sent = 1;
+                    }
+                }
         }
-
-        last;
         
-      }
+        close $fh;
     }
-  }
 }
 
 sub telegram_attention {
-  my $url = 'https://api.telegram.org/bot<bot_api>/sendMessage';
-  my $chat_id = '<chat id>';
-  my $text = 'wawning: cawtwidge or dwum status is less than 20%!!! please wepwace it!!! owo';
+    my $url = 'https://api.telegram.org/bot<bot_api>/sendMessage';
+    my $chat_id = '<chat id>';
+    my $text = 'wawning: cawtwidge or dwum status is less than 20%!!! please wepwace it!!! owo';
 
-  $chat_id =~ s/"/\\"/g;
-  $text =~ s/"/\\"/g;
-
-  my $command = qq{curl -s -X POST "$url" -d chat_id=$chat_id -d text=$text};
-
-  system($command);
-
+    $chat_id =~ s/"/\\"/g;
+    $text =~ s/"/\\"/g;
+    
+    my $command = qq{curl -s -X POST "$url" -d chat_id=$chat_id -d text=$text};
+    system($command);
 }
+
