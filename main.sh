d1cfbff2203c35b027a73996772547e804852a15
AnilAntari <retro201272@gmail.com>
2024-05-07 11:52:39 +0000

    Added Xerox Phaser 3610, Phaser 3635MFP, Phaser 4622, Phaser 6130N, WorkCentre 3220, WorkCentre 3225, WorkCentre 3315, WorkCentre 3325, WorkCentre 5222, WorkCentre 5230, WorkCentre 5335

--- main.sh
+++ main.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 
 # Xerox
-
+#/etc/uwuscan/mfd/Xerox/XeroxB215.pl
+#/etc/uwuscan/mfd/Xerox/XeroxPhaser3610.pl
+#/etc/uwuscan/mfd/Xerox/XeroxPhaser3635MFP.pl
+#/etc/uwuscan/mfd/Xerox/XeroxPhaser4622.pl
+#/etc/uwuscan/mfd/Xerox/XeroxPhaser6130N.pl
 #/etc/uwuscan/mfd/Xerox/XeroxVersaLinkB405.pl
+#/etc/uwuscan/mfd/Xerox/XeroxWorkCentre3220.pl
+#/etc/uwuscan/mfd/Xerox/XeroxWorkCentre3225.pl
+#/etc/uwuscna/mfd/Xerox/XeroxWorkCentre3315.pl
+#/etc/uwuscan/mfd/Xerox/XeroxWorkCentre3325.pl
 #/etc/uwuscan/mfd/Xerox/Xerox/WorkCentre3345.pl
 #/etc/uwuscan/mfd/Xerox/XeroxWorkCentre3615.pl
-#/etc/uuwscan/mfd/Xerox/XeroxB215.pl
+#/etc/uwuscan/mfd/Xerox/XeroxWorkCentre5222.pl
+#/etc/uwuscan/mfd/Xerox/XeroxWorkCentre5230.pl
+#/etc/uwuscan/mfd/Xerox/XeroxWorkCentre5335.pl
 
 # Telegram Notifications
 
---
+++ oid_list/Xerox/XeroxWorkCentre3315.pm
@@ -0,0 +1,7 @@
+package set;
+
+our  @oid_list = (
+  '1.3.6.1.2.1.43.11.1.1.8.1.1', # Cartridge max status 
+  '1.3.6.1.2.1.43.11.1.1.9.1.1'  # Cartridge current status  
+  );
+1;
