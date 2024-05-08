1bca6b6486e1db8300522c843671973efd7fb541
AnilAntari <86964331+AnilAntari@users.noreply.github.com>
2024-05-08 20:51:55 +0300

    Update attention.pl

--- attention.pl
+++ attention.pl
@@ -2,37 +2,41 @@
 
 use strict;
 use warnings;
+use autodie;
+use POSIX qw(strftime);
+use File::Basename;
 
 my $dir = '/var/uwuscan_log';
-my $num = 0;
+my $today = strftime "%a %b %e", localtime; # Date in the "Tue May 7" format
 
-for my $log_file (grep{-f}<$dir/*>) {
-    open (my $fh, '<', $log_file);
-    my @file = <$fh>;
-    close $fh;
+for my $log_file (glob "$dir/*") {
+    # Checking the file to make sure that this file is the right file
+    if (-f $log_file) {
+        # Opening a file for reading
+        open my $fh, '<', $log_file;
+        # Reading the file line by line
+        while (my $line = <$fh>) {
+            # attempt to extract data, cartridge and drum from a string
+            my ($date, $cawtwidge, $dwum) = $line =~ /(\[.*\]).*Cawtwidge: (\d+)%.*Dwum: (\d+)%/;
 
-    while (1) {
-        my $line = @file - 1 - $num;
-        my ($cawtwidge, $dwum) = $file[$line] =~ /Cawtwidge: (\d+)%.*Dwum: (\d+)%/;
-
-        if (!defined $cawtwidge || !defined $dwum) {
-            last;
+            # if the date is the same as the current one and at least one of the variables is less than 20
+            if ($date =~ /$today/ && defined $cawtwidge && defined $dwum && ($cawtwidge < 20 || $dwum < 20)) {
+                # Getting the file name
+                my $file_name = basename($log_file);
+                telegram_attention($file_name);
+                last;
+            }
         }
-
-        if ($cawtwidge < 20 || $dwum < 20) {
-            telegram_attention();
-            last;
-        }
-
-        ++$num;
-        last;
+        close $fh;
     }
 }
 
 sub telegram_attention {
+    my ($file_name) = @_;
+
     my $url = 'https://api.telegram.org/bot<token>/sendMessage';
     my $chat_id = '<chat id>';
-    my $text = 'wawning: cawtwidge or dwum status is less than 20%!!! please wepwace it!!! owo';
+    my $text = "wawning($file_name): cawtwidge or dwum status is less than 20%!!! please wepwace it!!! owo";
 
     my $command = qq{curl -s -X POST "$url" -d chat_id="$chat_id" -d text="$text"};
     system($command);
