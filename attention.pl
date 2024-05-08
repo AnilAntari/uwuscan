92334dde0a8a8ca888256b1a653cd3d58e52cca8
orazbaev.dias <orazbaev.dias@wb.ru>
2024-05-08 20:42:27 +0500

    refactor attention code more idiomatic

--- attention.pl
+++ attention.pl
@@ -6,28 +6,26 @@ use warnings;
 my $dir = '/var/uwuscan_log';
 my $num = 0;
 
-for my $log_file (grep{-f}<$dir/*>) {
-    open (my $fh, '<', $log_file);
-    my @file = <$fh>;
-    close $fh;
-
-    while (1) {
-        my $line = @file - 1 - $num;
-        my ($cawtwidge, $dwum) = $file[$line] =~ /Cawtwidge: (\d+)%.*Dwum: (\d+)%/;
-
-        if (!defined $cawtwidge || !defined $dwum) {
-            last;
+FILE: for my $log_file (glob "$dir/*") {
+    # проверяем, что файл существует и является файлом
+    if (-f $log_file) {
+        # открываем файл на чтение
+        open my $fh, '<', $log_file;
+        # читаем файл построчно
+        while (my $line = <$fh>) {
+            # если строка содержит "Cawtwidge: % и Dwum: %"
+            my ($cawtwidge, $dwum) = $line =~ /Cawtwidge: (\d+)%.*Dwum: (\d+)%/;
+            # если переменные определены и хотя бы одна из них меньше 20
+            if (defined $cawtwidge && defined $dwum && ($cawtwidge < 20 || $dwum < 20)) {
+                # отправляем сообщение в телеграм
+                telegram_attention();
+                last FILE;
+            }
         }
-
-        if ($cawtwidge < 20 || $dwum < 20) {
-            telegram_attention();
-            last;
-        }
-
-        ++$num;
-        last;
+        close $fh;
     }
 }
+
 
 sub telegram_attention {
     my $url = 'https://api.telegram.org/bot<token>/sendMessage';
