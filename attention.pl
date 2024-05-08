0facc6f6132edc5f6092b7ad97930feed0cbcde5
orazbaev.dias <orazbaev.dias@wb.ru>
2024-05-08 22:10:01 +0500

    add feature to check only today's log & add to report log file name

--- attention.pl
+++ attention.pl
@@ -2,35 +2,41 @@
 
 use strict;
 use warnings;
+use autodie;
+use POSIX qw(strftime);
+use File::Basename;
 
 my $dir = '/var/uwuscan_log';
-my $num = 0;
+my $today = strftime "%a %b %e", localtime; # получаем текущую дату в формате "Tue May 7"
 
-FILE: for my $log_file (glob "$dir/*") {
+for my $log_file (glob "$dir/*") {
     # проверяем, что файл существует и является файлом
     if (-f $log_file) {
         # открываем файл на чтение
         open my $fh, '<', $log_file;
         # читаем файл построчно
         while (my $line = <$fh>) {
-            # если строка содержит "Cawtwidge: % и Dwum: %"
-            my ($cawtwidge, $dwum) = $line =~ /Cawtwidge: (\d+)%.*Dwum: (\d+)%/;
-            # если переменные определены и хотя бы одна из них меньше 20
-            if (defined $cawtwidge && defined $dwum && ($cawtwidge < 20 || $dwum < 20)) {
-                # отправляем сообщение в телеграм
-                telegram_attention();
-                last FILE;
+            # пытаемся извлечь дату, cawtwidge и dwum из строки
+            my ($date, $cawtwidge, $dwum) = $line =~ /(\[.*\]).*Cawtwidge: (\d+)%.*Dwum: (\d+)%/;
+
+            # если дата совпадает с текущей и хотя бы одна из переменных меньше 20
+            if ($date =~ /$today/ && defined $cawtwidge && defined $dwum && ($cawtwidge < 20 || $dwum < 20)) {
+                # получаем имя файла
+                my $file_name = basename($log_file);
+                telegram_attention($file_name);
+                last;
             }
         }
         close $fh;
     }
 }
 
-
 sub telegram_attention {
+    my ($file_name) = @_;
+
     my $url = 'https://api.telegram.org/bot<token>/sendMessage';
     my $chat_id = '<chat id>';
-    my $text = 'wawning: cawtwidge or dwum status is less than 20%!!! please wepwace it!!! owo';
+    my $text = "wawning($file_name): cawtwidge or dwum status is less than 20%!!! please wepwace it!!! owo";
 
     my $command = qq{curl -s -X POST "$url" -d chat_id="$chat_id" -d text="$text"};
     system($command);
